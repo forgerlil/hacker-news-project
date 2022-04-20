f169bd38ee1676cafd35c37c226262b76f295025
Lilian Forger <lilianforger@gmail.com>
2022-04-20 17:06:31 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+      <div className="nav-links">
+        <div className="logo">Y</div>
+        <p><strong className="logo-text">Hacker News</strong><a>new</a> | <a>past</a> | <a>comments</a> | <a>ask</a> | <a>show</a> | <a>jobs</a>  | <a>submit</a></p>
+      </div>
+      <div className="login">login</div>
+    </nav>
+    <main>
+      <article>
+        <p>1.</p>
+        <img className="arrow-up" src="https://w7.pngwing.com/pngs/711/104/png-transparent-computer-icons-arrow-down-arrow-up-right-left-up-up-arrow-angle-triangle-black.png" />
+        <div className="article-content">
+          <p>Title of a given article here (<a>link.com</a>)</p>
+          <p className="article-links">??? points by <a>user1</a><a>time elapsed</a> | <a>hide</a> | <a>?? comments</a></p>
+        </div>
+      </article>
+      <article>
+        <p>1.</p>
+        <img className="arrow-up" src="https://w7.pngwing.com/pngs/711/104/png-transparent-computer-icons-arrow-down-arrow-up-right-left-up-up-arrow-angle-triangle-black.png" />
+        <div className="article-content">
+          <p>Title of a given article here (<a>link.com</a>)</p>
+          <p className="article-links">??? points by <a>user1</a><a>time elapsed</a> | <a>hide</a> | <a>?? comments</a></p>
+        </div>
+      </article>      <article>
+        <p>1.</p>
+        <img className="arrow-up" src="https://w7.pngwing.com/pngs/711/104/png-transparent-computer-icons-arrow-down-arrow-up-right-left-up-up-arrow-angle-triangle-black.png" />
+        <div className="article-content">
+          <p>Title of a given article here (<a>link.com</a>)</p>
+          <p className="article-links">??? points by <a>user1</a><a>time elapsed</a> | <a>hide</a> | <a>?? comments</a></p>
+        </div>
+      </article>      <article>
+        <p>1.</p>
+        <img className="arrow-up" src="https://w7.pngwing.com/pngs/711/104/png-transparent-computer-icons-arrow-down-arrow-up-right-left-up-up-arrow-angle-triangle-black.png" />
+        <div className="article-content">
+          <p>Title of a given article here (<a>link.com</a>)</p>
+          <p className="article-links">??? points by <a>user1</a><a>time elapsed</a> | <a>hide</a> | <a>?? comments</a></p>
+        </div>
+      </article>      <article>
+        <p>1.</p>
+        <img className="arrow-up" src="https://w7.pngwing.com/pngs/711/104/png-transparent-computer-icons-arrow-down-arrow-up-right-left-up-up-arrow-angle-triangle-black.png" />
+        <div className="article-content">
+          <p>Title of a given article here (<a>link.com</a>)</p>
+          <p className="article-links">??? points by <a>user1</a><a>time elapsed</a> | <a>hide</a> | <a>?? comments</a></p>
+        </div>
+      </article>
+    </main>
+    </>
   );
 }
 
